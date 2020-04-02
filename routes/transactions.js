b8ac237574b73bc1616735f0c2db16ff0c2c2e29
Lawrence Milton <milton.lawrence44@gmail.com>
2020-04-01 23:33:18 -0400

    Backend Api is completed

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getTransactions } = require("../controllers/transactions");
+const {
+  getTransactions,
+  addTransaction,
+  deleteTransaction
+} = require("../controllers/transactions");
 
-router.route("/").get(getTransactions);
+router
+  .route("/")
+  .get(getTransactions)
+  .post(addTransaction);
+
+router.route("/:id").delete(deleteTransaction);
 
 module.exports = router;
